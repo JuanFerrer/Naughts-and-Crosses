d91610966ea06dd3c580b848a688dad978979f04
JuanFerrer <juanemilioferrer@outlook.com>
2017-04-08 16:38:04 +0100

    Fixed RTL diagonal checking

--- Player.cs
+++ Player.cs
@@ -62,7 +62,7 @@ namespace NaughtsAndCrosses
             {
                 tokensInRowCount[t.x]++;
                 tokensInColCount[t.y]++;
-                
+
                 // If the amount of tokens in a row or column equals the amount of rows, we have a complete row or column
                 if (tokensInRowCount[t.x] == size || tokensInColCount[t.y] == size)
                     return true;
@@ -72,21 +72,30 @@ namespace NaughtsAndCrosses
             if (tokenPos.Contains(new Vector2(size / 2, size / 2)))
             {
                 // Ok, then check all diagonals
-                for (int i = 0; i < size; ++i)
-                {
-                    if (!tokenPos.Contains(new Vector2(i, i))) return false;
-                    if (!tokenPos.Contains(new Vector2(size - i, i))) return false;
-                }
-                return true;
+                if (IsDiagonal(size, true)) return true;
+                else if (IsDiagonal(size, false)) return true;
             }
             return false;
         }
-
         public List<Vector2> GetTokenPos() { return tokenPos; }
         public Token GetToken() { return token; }
         public int GetNumber() { return number; }
         public void SetToken(Token newToken) { token = newToken; }
         public void SetNumber(int newNum) { number = newNum; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="max">Size of board</param>
+        /// <param name="isLTR">Is left to right diagonal</param>
+        /// <returns></returns>
+        private bool IsDiagonal(int size, bool isLTR)
+        {
+            for (int i = 0; i < size; ++i)
+                if (!tokenPos.Contains(new Vector2(isLTR? i : (size - 1) - i, i))) return false;
+            return true;
+        }
 
         private List<Vector2> tokenPos;
         private Token token;
